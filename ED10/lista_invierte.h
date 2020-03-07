a3c1263da8d2219ebc560255d007c81f1f202721
CronopioRz <prabadan@ucm.es>
2020-03-07 18:17:48 +0100

    Ejercicio de decodificar el mensaje resuelto

--- ED10/lista_invierte.h
+++ ED10/lista_invierte.h
@@ -1,4 +1,5 @@
 #include"queue_eda.h"
+#include <iostream>
 #include <fstream>
 
 template <class T> 
@@ -17,14 +18,40 @@ class ListaInserta : public queue<T> {
         }
 
         //inserta un elemento en una lista enlazada simple
-        void inserta(Nodo const&n){
-            
+        void inserta(ListaInserta<T> &lista,int const& posIn){
+            //Nodo para guardar la direccion del elemento posterior a los insertados
+            Nodo * aux = this->prim;
+            int i = 0;
+            while (i < posIn){
+                if(i != 0)
+                    aux = aux->sig;
+                i++;
+            }
+            if (posIn == this->nelems) {  //Si se inserta al final
+                this->ult = lista.ult;
+            }
+            if (posIn > 0) {    
+                lista.ult->sig = aux->sig;                        // Si se
+                                            //inserta en el medio
+                aux->sig = lista.prim;
+            }
+            else {                         //Si se inserta al inicio
+                lista.ult->sig = aux;
+                this->prim = lista.prim;
+            }
+             this->nelems += lista.nelems;//Aumentamos el tamanyo de la nueva lista
+            vacia(lista);                //Vaciamos la lista auxiliar
+        }
+
+        void vacia(ListaInserta<T>&l){
+            l.prim=nullptr;
+            l.ult = nullptr;
+            l.nelems = 0; 
         }
        
-        friend std::ostream & operator<<(std::ostream & out, ListaInserta<T> const& l) {            
+        friend std::ostream & operator<<(std::ostream & out, ListaInserta<T>
+const& l) {            
             l.print(out);
             return out;
         }
 };
-
-
--- ED14/main.cpp
+++ ED14/main.cpp
@@ -6,48 +6,89 @@
 #include <iomanip>
 #include <fstream>
 #include "deque_eda.h"
+#include "stack_eda.h"
 
-template <typename T> 
-class list_decoder : public deque<T>{
-    private:
-        using Nodo = typename deque<char>::Nodo;
-    public:
-        void print(){
-            Nodo * aux  = this->fantasma->sig;
-            while(aux != this->fantasma) {
-                std::cout << aux->elem;
-                aux = aux->sig;
-            }
-                std::cout << std::endl;
-        }   
-        void XdPrima(){
+bool esVocal(const char& caracter) {
 
+	bool esVocal = false;
+
+	switch (caracter) {
+		case 'a': esVocal = true; break;
+		case 'A': esVocal = true; break;
+		case 'e': esVocal = true; break;
+		case 'E': esVocal = true; break;
+		case 'i': esVocal = true; break;
+		case 'I': esVocal = true; break;
+		case 'o': esVocal = true; break;
+		case 'O': esVocal = true; break;
+		case 'u': esVocal = true; break;
+		case 'U': esVocal = true; break;
+	}
+	return esVocal;
+}
+
+std::string resolver(std::string const& mensajeCodificado){
+    deque<char> cola;
+    stack<char> pila;
+    std::string mensajeDescodificado;
+
+    for(int i = 0; i < mensajeCodificado.size(); i++){
+        //Si es par se coloca en orden
+        if(i % 2 == 0) {
+            cola.push_back(mensajeCodificado[i]);
         }
-        void Xprima(){
-
+        //Si es impar se invierte con una pila 
+        else {
+            pila.push(mensajeCodificado[i]);
         }
+    }
+    //Metemos en la cola los elementos de la pila
+    while(!pila.empty()){
+        cola.push_back(pila.top());
+        pila.pop();
+    }
+    //Vaciamos la cola introducioendo los mensajes de forma ordenada en un string
+    while(!cola.empty()) {
+        //Si el valor es vocal se introduce en orden
+		if(esVocal(cola.front())) {
 
+			mensajeDescodificado += cola.front();
+			cola.pop_front();
 
+		}
+        //Si el valor es consonante se meten los valores siguientes en una pila
+        //hasta que se encuentre una vocal, para introducirlos de manera inversa 
+        else {
 
-};
+			while(!cola.empty() && !esVocal(cola.front())) {
 
+				pila.push(cola.front());
+				cola.pop_front();
 
+			}
+
+			while(!pila.empty()) {
+
+				mensajeDescodificado += pila.top();
+				pila.pop();
+
+			}
+
+		}
+
+	}
+    return mensajeDescodificado;
+}
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
     // leer los datos de la entrada
-    list_decoder<char> lCoded;
     std::string message;
     std::getline(std::cin,message);
     if (! std::cin)
         return false;
-    
-    
     // escribir sol
-    for(char &c: message){
-        lCoded.push_back(c);
-    }
-    lCoded.print();
+    std:: cout << resolver(message) << std::endl;
     return true;
     
 }
